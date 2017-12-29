63900d49900b727079ddc183182ffe5ddde8764c
Vitaly K <potyson@gmail.com>
2017-12-29 15:38:21 +0300

    Gg

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
-
-
 class Task:
     def __init__(self, time, stream_number):
         self.time = time
         self.time_left = None
         self.runtime = 0
         self.stream_number = stream_number
+        self.wait_time = 0
 
     def start(self, time, runtime):
+        if self.runtime > 0:
+            self.wait_time += time - self.time
         self.time_left = runtime
         self.time = time
 
@@ -35,6 +36,7 @@ class Stream:
 
     def add_task(self, task):
         self.queue.append(task)
+        self.time = task.time
         if task.time_left is None:
             self.input_task_counter += 1
 
@@ -57,22 +59,27 @@ class Server:
         self.streams = {}
         self.server_generators = server_generators
         self.server_generator_params = server_generator_params
+        self.input_task_list = []
         for number, priority in stream_config.items():
             self.streams[number] = Stream(priority)
 
     def add_task(self, task, stream_number):
+        self.input_task_list.append()
         if len(self.streams[stream_number].queue) == 0:
-            if self.busy and self.streams[stream_number].priority > self.streams[stream_number].priority:
+            if self.busy and self.streams[stream_number].priority > self.streams[self.task.stream_number].priority:
                 self.task.pause(task.time)
                 self.time = task.time
                 self.streams[task.stream_number].add_task(self.task)
                 self.task = None
                 self.busy = False
-            if not self.busy:
+            elif not self.busy:
                 task.start(self.time, self.server_generators[stream_number](*self.server_generator_params[stream_number]))
                 self.task = task
-                self.time += task.runtime
+                self.time += task.time_left
                 self.busy = True
+                self.streams[task.stream_number].time = task.time
+            else:
+                self.streams[stream_number].add_task(task)
         else:
             self.streams[stream_number].add_task(task)
 
@@ -119,13 +126,15 @@ class Server:
             self.output_task.append(self.task)
             self.task = None
         nearest_stream_time = self.get_nearest_stream_with_task_time()
+        self.time = nearest_stream_time
         if nearest_stream_time is not None:
             stream_numbers = self.get_stream_numbers_by_time(nearest_stream_time)
             stream_number = self.get_stream_number_with_high_priority(stream_numbers)
             task = self.streams[stream_number].get_task()
             task.start(self.time, self.server_generators[stream_number](*self.server_generator_params[stream_number]))
             self.task = task
-            self.time += self.task.runtime
+            self.time += self.task.time_left
+            self.busy = True
 
 
 class Model:
@@ -139,24 +148,29 @@ class Model:
         self.task_time = 0
         self.task_generators = task_generators
         self.task_generator_params = task_generator_params
+        self.task_counter = 0
 
     def start(self):
         self.system_time = 0
         for number, stream in self.server.streams.items():
+            self.task_counter += 1
             task_time = self.task_generators[number](*self.task_generator_params[number])
             task = Task(task_time, number)
             stream.add_task(task)
-            stream.time
+            stream.time = task_time
             if task_time < self.task_time or self.task_time == 0:
                 self.task_time = task_time
         while self.system_time <= self.runtime:
             if self.system_time >= self.task_time:
+                self.task_counter += 1
                 stream_numbers = self.server.get_stream_numbers_by_time(self.task_time)
                 for stream_number in stream_numbers:
-                    task = Task(self.task_generators[stream_number](*self.task_generator_params[stream_number]),
+                    task = Task(self.task_time + self.task_generators[stream_number](
+                                                                         *self.task_generator_params[stream_number]),
                                 stream_number)
                     self.server.add_task(task, stream_number)
             else:
                 self.server.next_task()
             self.task_time = self.server.get_nearest_stream_time()
             self.system_time = self.server.time
+        print("enter", self.task_counter, '; completed', len(self.server.output_task))
--- simulation.py
+++ simulation.py
@@ -13,11 +13,11 @@ def student_abs(nu):
     return abs(standard_t(nu))
 
 
-stream_config = {1: 1, 2: 1, 3: 1}
+stream_config = {1: 0, 2: 0, 3: 0}
 task_generators = {1: logistic_plus, 2: rayleigh, 3: gamma}
 task_generator_params = {1: (0, 1), 2: [1], 3: [2]}
 server_generators = {1: student_abs, 2: beta, 3: weibull}
-server_generator_params = {1: [1], 2: (0, 1), 3: [1]}
+server_generator_params = {1: [1], 2: (1, 2), 3: [1]}
 
-model1 = model.Model(1000, task_generators, task_generator_params, server_generators, server_generator_params, stream_config)
+model1 = model.Model(1000000, task_generators, task_generator_params, server_generators, server_generator_params, stream_config)
 model1.start()
